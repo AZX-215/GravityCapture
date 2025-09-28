bf20938fd421c4ef9428e41815112f41711f7742
AZX <125766660+AZX-215@users.noreply.github.com>
2025-09-28 00:52:59 -0400

    Update App.xaml.cs

--- src/GravityCapture/App.xaml.cs
+++ src/GravityCapture/App.xaml.cs
@@ -4,7 +4,7 @@ using GravityCapture.Services;
 
 namespace GravityCapture
 {
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
